80f48040be96ee2bf4967e89df4b60801deb5b25
mohamedmadboly110 <madbolymohamed690@gmail.com>
2025-08-27 19:00:46 +0300

    Initial commit - add problem solving files

---
+++ evenodd.js
@@ -0,0 +1,7 @@
+// Create a function that takes an integer as an argument and returns "Even" for even values or "Odd" for odd values.
+function evenOdd (value){
+    return value % 2== 0 ? 'even' : 'odd' ;
+}
+console.log(evenOdd(8))
+
+
