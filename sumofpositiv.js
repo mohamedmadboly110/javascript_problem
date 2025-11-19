c2ff4643f8b87021551af97925e238e7972f4937
mohamedmadboly110 <madbolymohamed690@gmail.com>
2025-11-19 16:02:20 +0200

    third problem

--- sumofpositiv.js
+++ sumofpositiv.js
@@ -2,7 +2,24 @@
 // Add the number to the initValue if Positive
 
 // يجمع الارقام الموجبة بس 
-function sumOfPositive(arr) {
+
+function sumOfPositive(arr){
+  let initValue = 0 
+  for(let i = 0; i< arr.length; i++){
+    if (arr[i]> 0){
+      initValue+= arr[i]
+    }
+
+  }
+  return initValue;
+}
+console.log(sumOfPositive([1 , 9 ,-4 , 9]))
+
+
+// another solution
+
+
+function sumPositive(arr) {
   return arr.filter((x) => x > 0).reduce((acc, current) => acc + current, 0);
 }
-console.log(sumOfPositive([1 , 2 , 9 ,4]))
+console.log(sumPositive([1 ,  9 , -4 , 9]))
